2b42c35955b980f6f80ce4950877e1d27a52fd98
Shinu999 <99495029+Shinu999@users.noreply.github.com>
2024-03-14 18:40:13 +0000

    02_datatype.js

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,15 @@
+let Name ="gaurav"
+let age = 18
+let city = "aligarh"
+let gf = true
+let a =3
+let b =5
+let sum =a+b
+let isLogedIn = true 
+
+
+console.log(typeof gf);
+console.log(typeof age);
+console.log( typeof isLogedIn);
+console.log(3+65);
+console.log(sum);
