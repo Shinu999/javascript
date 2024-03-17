b15ea0cdb91da21cd30f27440b0b5bbfa895ea64
Shinu999 <99495029+Shinu999@users.noreply.github.com>
2024-03-17 19:00:15 +0000

    05_string.js

---
+++ 01_basics/05_string.js
@@ -0,0 +1,22 @@
+const name = "gaurav"
+//console.log(name);
+
+const repoCount = 30 
+//console.log(repoCount);
+
+//console.log(`hyy my name is ${name} my repo Count num is ${repoCount}`);
+
+const gameName = new String ('gaurav')
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+const newString = gameName.substring(0, 4)
+console.log(newString);
+const anOtherStirng = gameName.slice(-3)
+console.log(anOtherStirng);
+
+
+
