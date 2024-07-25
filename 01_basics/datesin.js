fe97a8685549764419fc61de2eccfc6ae899e497
Shinu999 <99495029+Shinu999@users.noreply.github.com>
2024-07-25 08:29:06 +0000

    datesin.js

---
+++ 01_basics/datesin.js
@@ -0,0 +1,11 @@
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toTimeString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+
+const myCreateDate = new Date (2024,6,25);
+console.log(myCreateDate.toDateString());
