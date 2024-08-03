e95e0bb43941a471f4aa142c717d3f621ad76727
Shinu999 <99495029+Shinu999@users.noreply.github.com>
2024-08-03 17:06:31 +0000

    04_objects.js

---
+++ 01_basics/04_objects.js
@@ -0,0 +1,50 @@
+// singleton 
+
+//const tinderUser = new Object()
+
+// const tinderUser1 = {}     is a non singleton object
+//console.log(tinderUser1);
+
+const tinderUser = {}
+tinderUser.id = "123gfd"
+tinderUser.name = "gaurav"
+tinderUser.loggedIn = false
+ 
+//console.log(tinderUser);
+
+const regularUser = {
+    name : "Gaurav",
+    email : "gauravkumar@gmail.com",
+    loggedIn : true,
+    fullName : {
+        username : {
+            firstname : "shinu",
+            lastname : "nishad",
+        
+        }
+    }
+}
+
+//console.log(regularUser.fullName.username.firstname);
+
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+const obj4 = {5:"e", 6:"f"}
+
+//const obj3 = {obj1, obj2}
+
+//const obj3 = Object.assign({}, obj1,obj2,obj4)
+
+//const obj3 = {...obj1, ...obj2, ...obj4}
+//console.log(obj3);
+
+//console.log(tinderUser);
+//console.log(Object.keys(tinderUser));
+//console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('name'));
+
+
+
