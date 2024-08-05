3c33dae22b8fbab2e40aa916744ce7679f898b15
Shinu999 <99495029+Shinu999@users.noreply.github.com>
2024-08-05 11:33:55 +0000

    loops.js

---
+++ 01_basics/01_loop.js
@@ -0,0 +1,35 @@
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+    
+    
+// }
+
+
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log("gaurav");
+    
+    
+// }
+
+// let ip = 0;
+// let  house = 100;
+
+// while (ip != house ) {
+//     ip++
+//     console.log(ip);
+    
+// }
+
+
+let ip = 0;
+let  house = 40;
+
+do {
+    ip++
+    console.log(ip);
+} while (ip !== house); {
+   
+    // console.log(ip);
+    
+}
