12d9f0ccacbc06003da0cad5b0ae1395519c138d
Shinu999 <99495029+Shinu999@users.noreply.github.com>
2024-08-05 09:13:36 +0000

    switch.js

---
+++ 01_basics/01_ifelse.js
@@ -0,0 +1,9 @@
+const age =18 ;
+
+if (age >= 18) {
+    console.log("you are adult ");
+    
+} else {
+    console.log("you are child");
+    
+}
---
+++ 01_basics/01_swith.js
@@ -0,0 +1,34 @@
+
+// console.log(a);
+let a = 40;
+let option = '-'
+let b = 50
+
+ 
+switch (option) {
+     case "+":{ console.log(a + b);} 
+     break;
+
+     case "-": {console.log(a - b);}
+     break;
+
+     case "*":{ console.log(a * b);}
+     break;
+
+     default:{ console.log("invalid");}
+     
+     break;
+}
+
+// const options = 2 ;
+// switch (options) {
+//     case 1:console.log("namaste");
+//         break;
+
+//         case 2:console.log("hello");
+//         break;
+
+//     default: console.log("defoult");
+    
+//         break;
+// }
