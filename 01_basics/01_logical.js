8ce4ad47908b88541654e6609219d38025e36487
Shinu999 <99495029+Shinu999@users.noreply.github.com>
2024-08-05 10:40:40 +0000

    LogicalOperator

---
+++ 01_basics/01_logical.js
@@ -0,0 +1,23 @@
+// ============== AND =====================
+// const age = 12;
+// const gender = "male"
+
+// if (age >= 18 &&  gender == "male") {
+//     console.log(age );
+//     console.log(gender);  
+// }
+// ======================= OR =======================
+// const age = 20;
+// const gender = "male"
+
+// if (age >= 18 ||  gender == "male") {
+//     console.log(age );
+//     console.log(gender);  
+// }
+// ========================== NOT ==============
+const number = 19;
+ 
+if (!(number % 2 == 0) ) {
+    console.log("odd");
+    
+}
