dfa2e91166fb0c6ad46020ab2803bd5b51da053a
Shinu999 <99495029+Shinu999@users.noreply.github.com>
2024-08-14 11:51:04 +0000

    html

---
+++ weatherApp.html/script.js
@@ -0,0 +1,10 @@
+// alert("click");
+
+const button = document.getElementById ("search_button");
+const input =document.getElementById ("city_input")
+
+button.addEventListener ("click", () => {
+    // alert("clicked");
+    console.log(input.value);
+    
+}) ;
