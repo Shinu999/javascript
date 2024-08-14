814b0e9519dd49a3075165904a1bda1d2f9ab5b7
Shinu999 <99495029+Shinu999@users.noreply.github.com>
2024-08-14 14:59:34 +0000

    weather

--- weatherApp.html/script.js
+++ weatherApp.html/script.js
@@ -3,8 +3,21 @@
 const button = document.getElementById ("search_button");
 const input =document.getElementById ("city_input")
 
-button.addEventListener ("click", () => {
+ async function getData(cityName){
+  const promise = await fetch (
+        `http://api.weatherapi.com/v1/current.json?key=26955470b8bf4a859e790739241408&q=${cityName}&aqi=yes`);
+     return await promise.json()
+};
+
+
+button.addEventListener ("click", async () => {
     // alert("clicked");
-    console.log(input.value);
+    const value = input.value;
+     const result = await getData(value);
+     console.log(result);
+     
     
 }) ;
+
+
+//http://api.weatherapi.com/v1/current.json?key=26955470b8bf4a859e790739241408&q=London&aqi=yes
