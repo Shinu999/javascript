09787feca5059284a574fcc8289efdb160b102c5
Shinu999 <99495029+Shinu999@users.noreply.github.com>
2024-08-18 10:41:32 +0000

    iife

---
+++ 01_basics/01_Iife.js
@@ -0,0 +1,23 @@
+// function chai () {
+//     console.log(`DB CONNECTED`);
+    
+// }
+
+// chai()
+(function chai () {
+    console.log(`DB CONNECTED`);
+    
+}) ();
+
+// ( () {}) () 
+// () ()
+
+(()=>{
+    console.log(`DB CONNECTED TWO`);
+    
+})();
+
+((name)=>{
+    console.log(`DB CONNECTED TWO ${name}`);
+    
+})("Gaurav")
