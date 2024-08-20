4c22b3c22f3034ac3dae9be56a1dad3f830e270c
Shinu999 <99495029+Shinu999@users.noreply.github.com>
2024-08-20 19:45:21 +0000

    truthyvalue

---
+++ 01_basics/truthyvalue.js
@@ -0,0 +1,20 @@
+// const userEmail = "shinunishad@gmail.com"
+const userEmail = ""
+if (userEmail) {
+    console.log("you got email");   
+}
+else{
+    console.log("Don't have user email");
+    
+}  
+
+const array = []
+if (array.length === 0) {
+    console.log("array is empty");   
+}
+  
+
+const obj = []
+if (Object.keys(obj).length === 0) {
+    console.log("object is empty");   
+}
