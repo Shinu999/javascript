80775194d45e8a692e5e37857e1074077bc6caf2
Shinu999 <99495029+Shinu999@users.noreply.github.com>
2024-08-20 17:49:59 +0000

    controlflow

---
+++ 01_basics/controlflow.js
@@ -0,0 +1,68 @@
+// if 
+// let age = 18;
+
+// if (age >= 18) {
+//     console.log("are you adult ");
+    
+// }
+
+// else {
+//     console.log("are you chaild");
+    
+// }
+
+
+// if (  3 === "3") {
+//     console.log("are you smart ");
+    
+// }
+//  else {
+//     console.log("you are not smart");
+    
+//  }
+
+
+// const score = 200;
+
+// if (score < 100 ) {
+//     const power = "fly"
+//     console.log(`you are ${power}`);   
+// } 
+// else{
+//     console.log("you don`t have power");
+    
+// }
+
+// const balence = 1000
+
+// if (balence > 500) console.log("test");
+
+
+// 
+
+
+// const userLoggedIn = true
+// const dabitCard = true
+
+// if (userLoggedIn && dabitCard) {
+//     console.log("are you allow to shoping");
+// } 
+// else if (userLoggedIn){
+//     console.log("are you not allow");
+    
+// }else if (dabitCard){
+//     console.log("are you not allow");
+    
+// }
+// else{
+//     console.log("are you  not allow to shoping");
+    
+// }
+
+const userLoggedInEmail = false
+const userLoggedInGoogle = true
+
+if (userLoggedInEmail || userLoggedInGoogle) {
+    console.log("are you loggedIn ");
+    
+}
