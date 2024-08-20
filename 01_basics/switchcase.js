b79506ee783847fd6bb03797dac2c2b739da1fe4
Shinu999 <99495029+Shinu999@users.noreply.github.com>
2024-08-20 19:45:05 +0000

    switchcase

---
+++ 01_basics/switchcase.js
@@ -0,0 +1,14 @@
+const month = 4
+
+switch (month) {
+    case 1: console.log("jan");
+        break;
+        case 2: console.log("feb");
+        break;
+        case 3: console.log("mar");
+        break
+
+    default: console.log("default");
+    
+        break;
+}
