56e721df15a092b06be79ee393a21b12f5794b21
Shinu999 <99495029+Shinu999@users.noreply.github.com>
2024-08-21 08:14:49 +0000

    foreach

---
+++ 01_basics/forEach.js
@@ -0,0 +1,44 @@
+const heros = ["ironman", "superman", "flash","wonderWoman" , ]
+
+// heros.forEach(function (value) {
+//     console.log(value);
+    
+// })
+
+// heros.forEach ((value)=>{
+//     console.log(value);
+    
+// })
+
+// function printMe(value) {
+//     console.log(value);
+// }
+
+// heros.forEach(printMe)
+
+
+heros.forEach((value,index ,arr)=>{
+    // console.log(value,index ,arr);
+    
+})
+
+const myArray = [
+    {
+        lenguageName : "javaScript",
+        lenguageFileName : "js"
+    },
+    {
+        lenguageName : "java",
+        lenguageFileName : "java"
+    },
+    {
+        lenguageName : "Python",
+        lenguageFileName : "Py"
+    },
+]
+  
+
+myArray.forEach ((value)=>{
+       console.log(value.lenguageFileName);
+       
+})
