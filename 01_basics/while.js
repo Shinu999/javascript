e2a21cbf7e50d3745606b8cf380ff4585efc469a
Shinu999 <99495029+Shinu999@users.noreply.github.com>
2024-08-21 06:26:12 +0000

    while

---
+++ 01_basics/while.js
@@ -0,0 +1,19 @@
+let step = 0 ;
+while (step <= 10) {
+    // console.log(`step is ${step}`);
+    step= step+1   
+}
+
+let myArray = ["ironman ", "superman", "tonny", "flash"];
+ let arr = 0; 
+while (arr < myArray.length) {
+    // console.log(`my hero is ${myArray[arr]}`);
+    arr++;
+}
+
+
+let score = 1
+do {
+     console.log(`value is ${score}`);
+         score++;
+} while (score <= 10);
